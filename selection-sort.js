85f0306defdc12e27edbc9c7ab5c72f475ee3c17
Glen Selle <glenselle@me.com>
2017-01-29 17:04:13 -0600

    selection sort

---
+++ selection-sort.js
@@ -0,0 +1,21 @@
+
+Array.prototype.selectionSort = function(desc) {
+	let A = Object.assign([], this)
+	for(let j = 1; j < A.length; j++) {
+  	let key = A[j]
+    let i = j - 1
+
+    while(i >= 0 && (desc ? A[i] < key : A[i] > key)) {
+    	A[i + 1] = A[i]
+      i = i - 1
+     }
+
+    A[i + 1] = key
+  }
+  return A
+}
+
+/* -- Usage -- */
+let arr = [3, 8, 1, 6, 5, 4, 9, 2, 7]
+let ascending = arr.selectionSort()
+let descending = arr.selectionSort(true)
