9d415633751dbebcd4bca4bdd4e8ba06997c8c9d
Glen Selle <glenselle@me.com>
2017-01-29 20:10:29 -0600

    added a new selection sort algorithm

--- sorting.js
+++ sorting.js
@@ -1,7 +1,8 @@
+// Insertion Sort
 Array.prototype.insertionSort = function(desc) {
   let A = Object.assign([], this)
   for(let j = 1; j < A.length; j++) {
-  let key = A[j]
+    let key = A[j]
     let i = j - 1
 
     while(i >= 0 && (desc ? A[i] < key : A[i] > key)) {
@@ -14,7 +15,30 @@ Array.prototype.insertionSort = function(desc) {
   return A
 }
 
-/* -- Usage -- */
+// Usage
 let arr = [3, 8, 1, 6, 5, 4, 9, 2, 7]
 let ascending = arr.insertionSort() // [1,2,3,4,5,6,7,8,9]
 let descending = arr.insertionSort(true) // [9,8,7,6,5,4,3,2,1]
+
+
+// Selection sort
+Array.prototype.selectionSort = function(desc) {
+  let A = Object.assign([], this)
+  for(let j = 0; j < A.length; j++) {
+    let key = A[j]
+    let m = j
+
+    for(let i = j; i < A.length; i++) {
+      m = (desc ? A[i] > A[m] : A[i] < A[m]) ? i : m
+    }
+
+    A[j] = A[m]
+    A[m] = key
+  }
+  return A
+}
+
+// Usage
+let arr = [3, 8, 1, 6, 5, 4, 9, 2, 7]
+let ascending = arr.selectionSort() // [1,2,3,4,5,6,7,8,9]
+let descending = arr.selectionSort(true) // [9,8,7,6,5,4,3,2,1]
