6ce26156ff7e8635520aa1acfda41149cd19ee7d
Glen Selle <glenselle@me.com>
2017-01-29 19:36:27 -0600

    selectionSort was insertionSort -- name was wrong

--- sorting.js
+++ sorting.js
@@ -1,4 +1,4 @@
-Array.prototype.selectionSort = function(desc) {
+Array.prototype.insertionSort = function(desc) {
   let A = Object.assign([], this)
   for(let j = 1; j < A.length; j++) {
   let key = A[j]
@@ -16,5 +16,5 @@ Array.prototype.selectionSort = function(desc) {
 
 /* -- Usage -- */
 let arr = [3, 8, 1, 6, 5, 4, 9, 2, 7]
-let ascending = arr.selectionSort() // [1,2,3,4,5,6,7,8,9]
-let descending = arr.selectionSort(true) // [9,8,7,6,5,4,3,2,1]
+let ascending = arr.insertionSort() // [1,2,3,4,5,6,7,8,9]
+let descending = arr.insertionSort(true) // [9,8,7,6,5,4,3,2,1]
